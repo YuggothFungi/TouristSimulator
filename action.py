9a210c24999ac6f77eedd57f45723884d1337753
Nikita <nkharitonov@gmail.com>
2019-01-10 19:53:49 +0300

    call functions based on user input, functions calculate stamina based on duration

--- action.py
+++ action.py
@@ -1,7 +1,34 @@
-def player_action(action):
-    print("You " + action)
+class Player(object):
+    """Only player in the game"""
+
+    def __init__(self, stamina):
+        """Constructor"""
+        # self.isalive = True
+        self.stamina = stamina
 
 
-if __name__ == "__main":
-    act = input("Choose action")
-    player_action(act)
+def perform_sleep(duration):
+    player.stamina = player.stamina + 2*duration
+    print(player.stamina)
+
+
+def perform_run(duration):
+    player.stamina = player.stamina - 4*duration
+    print(player.stamina)
+
+
+def player_action(action, duration):
+    try:
+        hours = int(duration)
+        if action in action_map.keys():
+            action_map[action](hours)
+    except ValueError:
+        print("You made a mistake")
+
+action_map = {'sleep': perform_sleep, 'run': perform_run}
+
+if __name__ == "__main__":
+    player = Player(50)
+    act = input("Choose action ")
+    dur = input("Choose duration ")
+    player_action(act, dur)
