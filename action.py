ba8459562047e8da9e8be20ae96c3b1eb453c00c
Nikita <nkharitonov@gmail.com>
2019-01-10 21:40:50 +0300

    added comments

--- action.py
+++ action.py
@@ -1,5 +1,5 @@
 class Player(object):
-    """Only player in the game"""
+    """Only one player in the game"""
 
     def __init__(self, stamina):
         """Constructor"""
@@ -7,12 +7,21 @@ class Player(object):
         self.stamina = stamina
 
 
+"""Function declaration"""
 def perform_sleep(duration):
+    """
+    Function is called when 'sleep' action is selected
+    Sleeping increases stamina
+    """
     player.stamina = player.stamina + 2*duration
     print(player.stamina)
 
 
 def perform_run(duration):
+    """
+    Function is called when 'run' action is selected
+    Running decreases stamina
+    """
     player.stamina = player.stamina - 4*duration
     if player.stamina > 0:
         print(player.stamina)
@@ -22,16 +31,26 @@ def perform_run(duration):
 
 
 def player_action(action, duration):
+    """
+    Calls perform_<action> function with hours parameter ("duration" to int)
+    Need to get rid of this function after getting action-duration is changed from manual input to selection from enums
+    """
     try:
         hours = int(duration)
         if action in action_map.keys():
             action_map[action](hours)
+        else:
+            print("Invalid action")
     except ValueError:
-        print("You made a mistake")
+        print("Invalid duration")
 
+
+"""?Define global variables?"""
 action_map = {'sleep': perform_sleep, 'run': perform_run}
 
+
 if __name__ == "__main__":
+    """Create player and call for action while player is alive"""
     player = Player(50)
     while player.is_alive:
         act = input("Choose action ")
