057d29f6a6051fce41a297678d1676e7575aa500
Nikita <nkharitonov@gmail.com>
2019-01-13 22:12:50 +0300

    added eat action and function with meal types dict added conditional action call fixed player alive check when value is 0.0 refactored player_action function

--- action.py
+++ action.py
@@ -9,14 +9,14 @@ class Player(object):
 
 
 def player_alive(player_check):
-    if (player_check.satiety or player_check.stamina) <= 0:
+    if player_check.satiety <= 0.0 or player_check.stamina <= 0.0:
         player_check.is_alive = False
         print("Game over!")
 
 
 def update_player(player_update):
-    print(player_update.stamina)
-    print(player_update.satiety)
+    print("Stamina: " + str(player_update.stamina))
+    print("Satiety: " + str(player_update.satiety))
 
 
 def perform_sleep(duration):
@@ -24,8 +24,8 @@ def perform_sleep(duration):
     Function is called when 'sleep' action is selected
     Sleeping increases stamina
     """
-    player.stamina = player.stamina + 4*duration
-    player.satiety = player.satiety - 2*duration
+    player.stamina = player.stamina + 4 * duration
+    player.satiety = player.satiety - 2 * duration
 
     player_alive(player)
     if player.is_alive:
@@ -37,37 +37,58 @@ def perform_run(duration):
     Function is called when 'run' action is selected
     Running decreases stamina
     """
-    player.stamina = player.stamina - 4*duration
-    player.satiety = player.satiety - 4*duration
+    player.stamina = player.stamina - 4 * duration
+    player.satiety = player.satiety - 4 * duration
 
     player_alive(player)
     if player.is_alive:
         update_player(player)
 
 
-def player_action(action, duration):
+def perform_eat(meal_type):
+    """
+    Function is called when 'eat' action is selected
+    Eating increases satiety
+    To do: eating should take fixed duration depending on meal_type
+    """
+    player.satiety = player.satiety + meal_map[meal_type]
+    update_player(player)
+
+
+def player_action(action, duration=None, meal_type=None):
     """
     Calls perform_<action> function with hours parameter ("duration" to int)
     Need to get rid of this function after getting action-duration is changed from manual input to selection from enums
     """
-    try:
-        hours = int(duration)
-        if action in action_map.keys():
-            action_map[action](hours)
-        else:
-            print("Invalid action")
-    except ValueError:
-        print("Invalid duration")
+    if action in action_map.keys():
+        if duration is not None:
+            try:
+                hours = int(duration)
+                action_map[action](hours)
+            except ValueError:
+                print("Invalid duration")
+
+        if meal_type is not None:
+            if meal_type in meal_map.keys():
+                action_map[action](meal_type)
+            else:
+                print("Invalid action type")
+    else:
+        print("Invalid action")
 
 
 """?Define global variables?"""
-action_map = {'sleep': perform_sleep, 'run': perform_run}
-
+action_map = {'sleep': perform_sleep, 'run': perform_run, 'eat': perform_eat}
+meal_map = {'fast': 10.0, 'break': 10.0, 'lunch': 15.0, 'dinner': 20.0, 'feast': 30.0}
 
 if __name__ == "__main__":
     """Create player and call for action while player is alive"""
     player = Player(50.0, 50.0)
     while player.is_alive:
         act = input("Choose action ")
-        dur = input("Choose duration ")
-        player_action(act, dur)
+        if act == 'eat':
+            eat = input("Choose meal type ")
+            player_action(act, None, eat)
+        else:
+            dur = input("Choose duration ")
+            player_action(act, dur, None)
